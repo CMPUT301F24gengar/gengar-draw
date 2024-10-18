8aab86c45bfe9b8e06d9a40a3b27f93131c8ddcb
Rafiul Alam Khan <156750393+rafi-khan-cmd@users.noreply.github.com>
2024-10-18 00:38:56 -0600

    Delete app/src/main/java/com/example/gengardraw/Admin.java

--- app/src/main/java/com/example/gengardraw/Admin.java
+++
@@ -1,9 +0,0 @@
-package com.example.gengardraw;
-
-public class Admin extends UserProfile{
-    public Admin(String deviceId){
-        super(deviceId,null, null, null, null,null,true, false,false);
-    }
-
-
-}
