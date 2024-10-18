342755e7fc964a5c404585cd211a73844c26ff29
Rafiul Alam Khan <156750393+rafi-khan-cmd@users.noreply.github.com>
2024-10-18 00:39:23 -0600

    Delete app/src/main/java/com/example/gengardraw/Organizer.java

--- app/src/main/java/com/example/gengardraw/Organizer.java
+++
@@ -1,7 +0,0 @@
-package com.example.gengardraw;
-
-public class Organizer extends UserProfile{
-    public Organizer(String deviceID){
-        super(deviceID, null,null,null, null,null,false,true,false);
-    }
-}
