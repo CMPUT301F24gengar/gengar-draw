623fe678d51db472eef63fe1ccac850a2fd8fe33
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2024-11-01 19:30:47 -0600

    cleaned up comments and variable definitions

--- app/src/main/java/Fragments/facility_profile.java
+++ app/src/main/java/Fragments/facility_profile.java
@@ -36,44 +36,16 @@ import Classes.UserProfile;
 import Classes.UserProfileManager;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link facility_profile#newInstance} factory method to
- * create an instance of this fragment.
+ * facility profile fragment
  */
 public class facility_profile extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment facility_profile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static facility_profile newInstance(String param1, String param2) {
-        facility_profile fragment = new facility_profile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     //data
+    String deviceID;
     private static final int PICK_IMAGE = 1000;//request code for image gallery
     private Uri ImageURI = null;
     private String ImageURI_string;
-    //private UserProfile user;
-    String deviceID;
+
+    //view references;
     private ImageView facilityImage;
     private EditText nameEditText, locationEditText, descriptionEditText;
     private TextView createUpdateBtn, cancelBtn, addFacilityImage, removeFacilityImage;
