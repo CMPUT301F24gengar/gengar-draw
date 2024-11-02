bce6396a7070e9f3270d04bbfe24917b429f8662
RehanShanavas <rehanshanavas@outlook.com>
2024-11-02 13:39:17 -0600

    Added a working admin panel with checks

--- app/src/main/java/com/example/gengardraw/MainActivity.java
+++ app/src/main/java/com/example/gengardraw/MainActivity.java
@@ -20,8 +20,10 @@ import androidx.fragment.app.FragmentTransaction;
 
 import Classes.UserProfile;
 import Classes.UserProfileManager;
+import Fragments.admin_user_profiles;
 import Fragments.create_event;
 import Fragments.facility_profile;
+import Fragments.my_events;
 import Fragments.notifications;
 import Fragments.qr_scanner;
 import Fragments.register;
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements register.OnRegist
     FrameLayout adminLayout;
     ImageView adminButton;
     LinearLayout adminButtonsExpanded;
-    ImageView adminProfileButton;
+    ImageView adminUserProfileButton;
     ImageView adminEventsButton;
     ImageView adminImagesButton;
     ImageView adminFacilitiesButton;
@@ -96,27 +98,32 @@ public class MainActivity extends AppCompatActivity implements register.OnRegist
         navbarMyEventsbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedButton(navbarMyEventsbutton);
+                setHighlightedButton(navbarMyEventsbutton, false);
+                setHighlightedAdminButton(adminButton, isExpanded, true);
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new my_events()).commit();
             }
         });
         navbarScannerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedButton(navbarScannerbutton);
+                setHighlightedButton(navbarScannerbutton, false);
+                setHighlightedAdminButton(adminButton, isExpanded, true);
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new qr_scanner()).commit();
             }
         });
         navbarCreateEventbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedButton(navbarCreateEventbutton);
+                setHighlightedButton(navbarCreateEventbutton,false);
+                setHighlightedAdminButton(adminButton, isExpanded, true);
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new create_event()).commit();
             }
         });
         navbarProfilebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedButton(navbarProfilebutton);
+                setHighlightedButton(navbarProfilebutton,false);
+                setHighlightedAdminButton(adminButton, isExpanded, true);
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new user_profile()).commit();
             }
         });
@@ -124,7 +131,7 @@ public class MainActivity extends AppCompatActivity implements register.OnRegist
         adminLayout = findViewById(R.id.admin_layout);
         adminButton = findViewById(R.id.admin_button);
         adminButtonsExpanded = findViewById(R.id.admin_buttons_expanded);
-        adminProfileButton = findViewById(R.id.admin_profile_button);
+        adminUserProfileButton = findViewById(R.id.admin_profile_button);
         adminEventsButton = findViewById(R.id.admin_events_button);
         adminImagesButton = findViewById(R.id.admin_images_button);
         adminFacilitiesButton = findViewById(R.id.admin_facilities_button);
@@ -135,63 +142,71 @@ public class MainActivity extends AppCompatActivity implements register.OnRegist
             @Override
             public void onClick(View v) {
                 if (!isExpanded) { // then expand the buttons panel
-                    setHighlightedAdminButton(adminButton, isExpanded);
+                    setHighlightedAdminButton(adminButton, isExpanded, false);
                     adminButtonsExpanded.setVisibility(View.VISIBLE);
                     isExpanded = true;
                 } else {
-                    setHighlightedAdminButton(adminButton, isExpanded);
+                    setHighlightedAdminButton(adminButton, isExpanded, false);
                     adminButtonsExpanded.setVisibility(View.GONE);
                     isExpanded = false;
                 }
             }
         });
 
-        adminProfileButton.setOnClickListener(new View.OnClickListener() {
+        adminUserProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedAdminButton(adminProfileButton, isExpanded);
+                setHighlightedAdminButton(adminUserProfileButton, isExpanded, false);
+                setHighlightedButton(highlightedButton, true);
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new admin_user_profiles()).commit();
             }
         });
         adminEventsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedAdminButton(adminEventsButton, isExpanded);
+                setHighlightedAdminButton(adminEventsButton, isExpanded, false);
             }
         });
         adminImagesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedAdminButton(adminImagesButton, isExpanded);
+                setHighlightedAdminButton(adminImagesButton, isExpanded, false);
             }
         });
         adminFacilitiesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setHighlightedAdminButton(adminFacilitiesButton, isExpanded);
+                setHighlightedAdminButton(adminFacilitiesButton, isExpanded, false);
             }
         });
-
     }
 
-    private void setHighlightedButton(ImageView button) {
+    private void setHighlightedButton(ImageView button, boolean unhighlight) {
         highlightedButton.setColorFilter(ContextCompat.getColor(this, R.color.grey));
         highlightedButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black2)));
-        highlightedButton = button;
-        highlightedButton.setColorFilter(ContextCompat.getColor(this, R.color.black2));
-        highlightedButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.green)));
+        if (!unhighlight) { // set the button to highlighted
+            highlightedButton = button;
+            highlightedButton.setColorFilter(ContextCompat.getColor(this, R.color.black2));
+            highlightedButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.green)));
+        }
     }
 
-    private void setHighlightedAdminButton(ImageView button, boolean isExpanded) {
+    private void setHighlightedAdminButton(ImageView button, boolean isExpanded, boolean unhighlight) {
+        highlightedAdminButton.setColorFilter(ContextCompat.getColor(this, R.color.grey));
+        highlightedAdminButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black2)));
+        highlightedAdminButton = button;
         if (!isExpanded) { // make admin button unhighlighted
-            highlightedAdminButton.setColorFilter(ContextCompat.getColor(this, R.color.grey));
-            highlightedAdminButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black2)));
-            highlightedAdminButton = button;
+            if (unhighlight) {
+                highlightedAdminButton.setColorFilter(ContextCompat.getColor(this, R.color.black2));
+                highlightedAdminButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.red)));
+                return;
+            }
             highlightedAdminButton.setColorFilter(ContextCompat.getColor(this, R.color.grey));
             highlightedAdminButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black2)));
         } else {
-            highlightedAdminButton.setColorFilter(ContextCompat.getColor(this, R.color.grey));
-            highlightedAdminButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black2)));
-            highlightedAdminButton = button;
+            if (unhighlight) {
+                return;
+            }
             highlightedAdminButton.setColorFilter(ContextCompat.getColor(this, R.color.black2));
             highlightedAdminButton.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.red)));
         }
@@ -204,7 +219,6 @@ public class MainActivity extends AppCompatActivity implements register.OnRegist
 
     @Override
     public void loadMainContentFragment(UserProfile userProfile) {
-        // TODO : Replace the main_content FrameLayout with the main content and set the register FrameLayout visibility to false
         registerFrame.setVisibility(View.GONE);
         getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new notifications()).commit();
         this.userProfile = userProfile;
@@ -214,7 +228,8 @@ public class MainActivity extends AppCompatActivity implements register.OnRegist
     }
 
     public void showHomeFragment() {
-        setHighlightedButton(navbarNotificationsbutton);
+        setHighlightedButton(navbarNotificationsbutton, false);
+        setHighlightedAdminButton(adminButton, isExpanded, true);
         getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new notifications()).commit();
     }
 
