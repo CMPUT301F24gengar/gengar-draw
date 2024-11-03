81f9b6535841a9e8d8fadab57cadd6efc353aada
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2024-11-03 13:00:26 -0700

    made basic methods for hosted_events fragment class

--- app/src/main/java/Fragments/hosted_events.java
+++ app/src/main/java/Fragments/hosted_events.java
@@ -1,6 +1,46 @@
 package Fragments;
 
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gengardraw.MainActivity;
+import com.example.gengardraw.R;
+
+/**
+ * "Hosted Events" fragment hander
+ */
 public class hosted_events extends Fragment {
+    //activity views
+    //data
+    public hosted_events(){
+        //required empty constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_events, container, false);//TODO use recycler view to fill listview correctly
+    }
+    private void closeFragment() {
+        if (getActivity() instanceof MainActivity) {
+            MainActivity activity = (MainActivity) getActivity();
+            activity.showHomeFragment();
+        } else {
+            // Handle error
+        }
+    }
+    // replace current fragment with Joined Events fragment
+    private void openJoinedEventsFragment() {
+        if (getActivity() instanceof MainActivity) {
+            MainActivity activity = (MainActivity) getActivity();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new my_events()).commit();
+        } else {
+            // Handle error
+        }
+    }
 }
