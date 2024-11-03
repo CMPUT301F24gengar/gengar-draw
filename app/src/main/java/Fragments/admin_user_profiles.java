db6af462c5bbc9a7e72fe393531029531d0101e9
Dion <dionalexmathew@gmail.com>
2024-11-02 20:22:18 -0600

    Updated admin_user_profile to create arraylist of userprofiles from the userprofiles stored in firebase.

--- app/src/main/java/Fragments/admin_user_profiles.java
+++ app/src/main/java/Fragments/admin_user_profiles.java
@@ -2,65 +2,93 @@ package Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gengardraw.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link admin_user_profiles#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+import Classes.UserProfile;
+import Classes.UserProfileManager;
+
 public class admin_user_profiles extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public admin_user_profiles() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment admin_user_profiles.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static admin_user_profiles newInstance(String param1, String param2) {
-        admin_user_profiles fragment = new admin_user_profiles();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FirebaseFirestore db;
+    private UserProfileManager userProfileManager;
+    ArrayList<UserProfile> userProfiles = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_user_profiles, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_user_profiles, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        userProfileManager= new UserProfileManager();
+
+        fetchUserProfiles(new OnProfilesLoadedListener() {
+            @Override
+            public void onProfilesLoaded(ArrayList<UserProfile> userProfiles) {
+                for (int i=0;i<userProfiles.size();i++){
+                    Log.d("userprofilename","name: "+userProfiles.get(i).getName());
+                }
+            }
+        });
+
+        return view;
+
     }
+
+    public interface OnProfilesLoadedListener {
+        void onProfilesLoaded(ArrayList<UserProfile> userProfiles);
+    }
+
+    //creates listener since firebase's get() is asynchronous in nature,
+    //so it notifies when all profiles have been loaded.
+    public void fetchUserProfiles(OnProfilesLoadedListener listener) {
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("userprofile", document.getId() + " => " + document.getData());
+
+                                String currentDeviceID = document.getId();
+
+                                userProfileManager.getUserProfile(currentDeviceID, new UserProfileManager.OnUserProfileFetchListener() {
+                                    @Override
+                                    public void onUserProfileFetched(UserProfile userProfile) {
+                                        userProfiles.add(userProfile);
+                                        // Check if all documents have been processed
+                                        if (userProfiles.size() == task.getResult().size()) {
+                                            listener.onProfilesLoaded(userProfiles); // Notify when done
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onUserProfileFetchError(Exception e) {
+                                        Log.d("userProfiles fetch ERROR", e.toString());
+                                    }
+                                });
+                            }
+                        } else {
+                            Log.d("userprofile doc", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
 }
