7fc9044a3f7e55f6796c679617edb8018d673309
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2024-11-29 12:30:38 -0700

    added check of if event editable on image selection

--- app/src/main/java/Fragments/update_event.java
+++ app/src/main/java/Fragments/update_event.java
@@ -164,9 +164,10 @@ public class update_event extends Fragment {
         });
 
         eventPicture.setOnClickListener(view1 -> {
+            if (isEditable.get()){
             Intent OpenGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(OpenGalleryIntent, 1000);
-            eventPictureUpdated = true;
+            eventPictureUpdated = true;}
         });
 
         updateEventSaveBtn.setOnClickListener(new View.OnClickListener() {
