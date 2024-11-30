20f1771ee7654741d57313f619b8fb0d0ec2ffe7
Dion <dionalexmathew@gmail.com>
2024-11-29 19:41:48 -0700

    Fixed bug in admin_qrcode

--- app/src/main/java/Fragments/admin_qrcodes.java
+++ app/src/main/java/Fragments/admin_qrcodes.java
@@ -57,6 +57,7 @@ public class admin_qrcodes extends Fragment {
     private EditText searchBarText;
     private String searchQuery;
     private EventAdapter.OnEventClickListener eventsListener;
+    private int count;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -74,9 +75,10 @@ public class admin_qrcodes extends Fragment {
         layoutManager = new LinearLayoutManager(getActivity());
         customAdapter = new QRcodeAdapter(getContext(),events, true);
         searchCustomAdapter = new QRcodeAdapter(getContext(),searchEvents, true);
+        count = 0;
 
 
-        fetchEvents(new admin_events.OnEventsLoadedListener() {
+        fetchEvents(new OnEventsLoadedListener() {
             @Override
             public void onEventsLoaded(ArrayList<Event> events) {
                 //adding userProfiles to adapter
@@ -110,7 +112,7 @@ public class admin_qrcodes extends Fragment {
 
     //creates listener since firebase's get() is asynchronous in nature,
     //so it notifies when all profiles have been loaded.
-    public void fetchEvents(admin_events.OnEventsLoadedListener listener) {
+    public void fetchEvents(OnEventsLoadedListener listener) {
         db.collection("events")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -132,9 +134,15 @@ public class admin_qrcodes extends Fragment {
                                         if(event.getQRCode() != null){
                                             events.add(event);
                                         }
+                                        count++;
                                         // Check if all documents have been processed
-                                        if (events.size() == task.getResult().size()) {
+                                        if (count == task.getResult().size()) {
+                                            Log.d("qrcode events","peepee");
                                             listener.onEventsLoaded(events); // Notify when done
+                                        }
+                                        else{
+                                            Log.d("12345","event size: "+events.size());
+                                            Log.d("12345","event size: "+task.getResult().size());
                                         }
                                     }
 
@@ -150,113 +158,4 @@ public class admin_qrcodes extends Fragment {
                     }
                 });
     }
-
-
-
-//    private FirebaseFirestore db;
-//    private RecyclerView recyclerView;
-//    private QRcodeManager qrCodeManager;
-//    private ArrayList<QRcode> qrCodes;
-//    private ArrayList<String> qrCodesId;
-//    private ArrayList<QRcode> searchQRCodes;
-//    private ArrayList<String> searchQRCodesId;
-//    private RecyclerView.LayoutManager layoutManager;
-//    private QRcodeAdapter customAdapter;
-//    private QRcodeAdapter searchCustomAdapter;
-//    private ImageView searchButton;
-//    private EditText searchBarText;
-//    private String searchQuery;
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.fragment_admin_qrcodes, container, false);
-//
-//        db = FirebaseFirestore.getInstance();
-//        recyclerView = view.findViewById(R.id.admin_qrcodes_list);
-//        searchButton = view.findViewById(R.id.admin_qrcode_search_button);
-//        searchBarText = view.findViewById(R.id.admin_qrcode_search_bar);
-//        qrCodeManager = new QRcodeManager();
-//        qrCodes = new ArrayList<>();
-//        qrCodesId = new ArrayList<>();
-//        searchQRCodes = new ArrayList<>();
-//        searchQRCodesId = new ArrayList<>();
-//        layoutManager = new LinearLayoutManager(getActivity());
-//        customAdapter = new QRcodeAdapter(getContext(),qrCodesId, true);
-//        searchCustomAdapter = new QRcodeAdapter(getContext(),searchQRCodesId, true);
-//
-//
-//        fetchQRcodes(new OnQRCodeLoadedListener() {
-//            @Override
-//            public void onQRCodeLoaded(ArrayList<String> qrCodes) {
-//                //adding qrCodes to adapter
-//                Log.d("size in : ","size: " + qrCodes.size());
-//                recyclerView.setLayoutManager(layoutManager); //arranges recyclerView in linear form
-//                recyclerView.setAdapter(customAdapter);
-//            }
-//        });
-//
-//        searchButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                searchQRCodesId.clear(); //in case it is filled from a search before.
-//                searchQuery = searchBarText.getText().toString(); //get text from searchbar edittext
-//                //query with the text
-//                for (int i=0; i<qrCodesId.size();i++){
-//                    if(qrCodesId.get(i).toLowerCase().contains(searchQuery.toLowerCase())){
-//                        searchQRCodesId.add(qrCodesId.get(i));
-//                    }
-//                }
-//                searchCustomAdapter.notifyDataSetChanged();
-//                recyclerView.setAdapter(searchCustomAdapter);
-//            }
-//        });
-//
-//        return view;
-//    }
-//
-//    /**
-//     * interface for listener to check if all the QRCodes have been loaded.
-//     */
-//    public interface OnQRCodeLoadedListener {
-//        void onQRCodeLoaded(ArrayList<String> qrCodes);
-//    }
-//
-//    /**
-//     * This method fetches all the qr codes from firebase currently stored
-//     * and adds it to a list of qr codes.
-//     * @param listener to check if all the qr codes have been loaded.
-//     */
-//    //creates listener since firebase's get() is asynchronous in nature,
-//    //so it notifies when all profiles have been loaded.
-//    public void fetchQRcodes(OnQRCodeLoadedListener listener) {
-//        db.collection("qrcodes")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            if (task.getResult().isEmpty()) {
-//                                listener.onQRCodeLoaded(qrCodesId);
-//                                return;
-//                            }
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                Log.d("qrcode", document.getId() + " => " + document.getData());
-//
-//                                String qrCodeID = document.getId();
-//                                Log.d("qrcodeID","QRCODEID: "+qrCodeID);
-//                                qrCodesId.add(qrCodeID);
-//                                if(qrCodesId.size() == task.getResult().size()){
-//                                    listener.onQRCodeLoaded(qrCodesId);
-//                                    Log.d("qrcode loaded","loaded");
-//                                }
-//                            }
-//                        } else {
-//                            Log.d("qrcode doc", "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-//    }
-
 }
