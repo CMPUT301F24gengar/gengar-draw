0ed3c8fb31bbbf6b3b60b806cf2d2ba563128eef
RehanShanavas <rehanshanavas@outlook.com>
2024-12-01 00:56:55 -0700

    Added javadocs for notification manager

--- app/src/main/java/Adapters/NotificationAdapter.java
+++ app/src/main/java/Adapters/NotificationAdapter.java
@@ -18,6 +18,9 @@ import java.util.List;
 import Classes.Notification;
 import Classes.NotificationManager;
 
+/**
+ * This is the NotificationAdapter which is a custom adapter to display all the notifications.
+ */
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MyViewHolder> {
 
     private Context context;
@@ -25,6 +28,13 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
     private String userID;
     private Boolean showDelete;
 
+    /**
+     * Constructor for NotificationAdapter done with context and an arraylist of notifications.
+     * @param context the context in which the adapter is working.
+     * @param notifications the list to be displayed in the adapter.
+     * @param userID the user's id
+     * @param showDelete the delete button
+     */
     public NotificationAdapter(Context context, ArrayList<Notification> notifications, String userID, Boolean showDelete) {
         this.context=context;
         this.localNotifications = notifications;
@@ -32,6 +42,14 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         this.showDelete = showDelete;
     }
 
+    /**
+     * Called when the RecyclerView needs a new viewHolder.
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     *               an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return the new viewHolder
+     */
     @NonNull
     @Override
     public NotificationAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -39,6 +57,12 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         return new NotificationAdapter.MyViewHolder(row);
     }
 
+    /**
+     * Binds the data from the notification at a specified position to the view holder.
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     *        item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull NotificationAdapter.MyViewHolder holder, int position) {
         holder.delete.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +94,9 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         holder.title.setText(notification.getEventTitle());
     }
 
+    /**
+     * Used to get references for views of a single item.
+     */
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView message;
         TextView day;
@@ -89,6 +116,9 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         }
     }
 
+    /**
+     * @return Gets the total count of localNotifications.
+     */
     @Override
     public int getItemCount() {
         return localNotifications.size();
