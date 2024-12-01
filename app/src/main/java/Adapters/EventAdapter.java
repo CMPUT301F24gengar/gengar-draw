a731ab0443207c6869dc32a4b0b8b55e29f41f98
Rheanna365 <rglass@ualberta.ca>
2024-11-30 17:52:03 -0700

    change EventAdapter to match development branch

--- app/src/main/java/Adapters/EventAdapter.java
+++ app/src/main/java/Adapters/EventAdapter.java
@@ -10,34 +10,30 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.example.gengardraw.R;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
 import java.util.Locale;
 
 import Classes.Event;
+import Classes.Facility;
 import Classes.FacilityManager;
 import Classes.EventManager;
 import Classes.UserProfile;
 
-public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder> {
 
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder> {
     private Context context;
     private List<Event> localEvents;
     private Boolean showDelete;
     private OnEventClickListener listener;
     private String facilityName;
     private String facilityPictureURL;
-
     public EventAdapter(Context context, ArrayList<Event> events, Boolean showDelete, OnEventClickListener listener) {
         this.context=context;
         localEvents = events;
@@ -46,27 +42,22 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
         this.facilityName = "";
         this.facilityPictureURL = "";
     }
-
     public void setFacilityName(String facilityName) {
         this.facilityName = facilityName;
         notifyDataSetChanged();
     }
-
     public void setFacilityPicture(String facilityPictureURL) {
         this.facilityPictureURL = facilityPictureURL;
         notifyDataSetChanged();
     }
-
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View row = LayoutInflater.from(context).inflate(R.layout.event_item, parent, false);
         return new MyViewHolder(row);
     }
-
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
         holder.Delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,31 +71,39 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
         });
 
         Event event = localEvents.get(position);
-        String eventID = localEvents.get(position).getEventID();
         holder.Delete.setVisibility(showDelete ? View.VISIBLE : View.GONE);
-        holder.facilityNameTextView.setText(facilityName);
-        Log.d("EventAdapter", "onBindViewHolder FacilityPictureURL: " + facilityPictureURL);
+        String organizerID = event.getOrganizerID();
+        FacilityManager facilityManager = new FacilityManager();
+        facilityManager.getFacility(organizerID, new FacilityManager.OnFacilityFetchListener() {
+            @Override
+            public void onFacilityFetched(Facility facility) {
+                //set image
+                if (facility.getPictureURL() == null){
+                    holder.facilityPicture.setImageDrawable(context.getResources().getDrawable(R.drawable.user));
+                }
+                else{
+                    holder.facilityPicture.setImageTintList(null);
+                    Glide.with(context).load(facility.getPictureURL()).into(holder.facilityPicture);
+                }
 
-        if (facilityPictureURL == null) {
-            holder.facilityPicture.setImageDrawable(context.getResources().getDrawable(R.drawable.user));
-            holder.facilityPicture.setImageTintList(context.getResources().getColorStateList(R.color.green));
-        } else if (!facilityPictureURL.isEmpty()) {
-            holder.facilityPicture.setImageTintList(null);
-            Glide.with(context).load(facilityPictureURL).into(holder.facilityPicture);
-        } //else {
-//            holder.facilityPicture.setImageDrawable(context.getResources().getDrawable(R.drawable.user));
-//            holder.facilityPicture.setImageTintList(context.getResources().getColorStateList(R.color.green));
-//        }
+                //set text to facility name
+                holder.facilityNameTextView.setText(facility.getName());
+            }
+
+            @Override
+            public void onFacilityFetchError(Exception e) {
+                Log.d("Facility fetch error eventadapter: ", e.toString());
+            }
+        });
         holder.eventTitle.setText(event.getEventTitle());
         holder.eventStartDay.setText(String.valueOf(event.getEventStartDate().getDate()));
         holder.eventStartMonth.setText(new SimpleDateFormat("MMM", Locale.getDefault()).format(event.getEventStartDate()).toUpperCase());
         holder.eventStartTime.setText(new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(event.getEventStartDate()).toUpperCase());
         if (event.getEventPictureURL() != null) {
-            holder.eventPicture.setImageTintList(null);
             Glide.with(context).load(event.getEventPictureURL()).into(holder.eventPicture);
+            holder.eventPicture.setVisibility(View.VISIBLE);
         } else {
-            holder.eventPicture.setImageDrawable(context.getResources().getDrawable(R.drawable.user));
-            holder.eventPicture.setImageTintList(context.getResources().getColorStateList(R.color.green));
+            holder.eventPicture.setVisibility(View.GONE);
         }
         holder.itemView.setOnClickListener(v -> {
             if (listener != null) {
@@ -112,45 +111,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
                 Log.d("EventAdapter", "setOnClickListener position: " + position + " event title: " + event.getEventTitle());
             }
         });
-
-        // Joined Events don't have facility names passed to the adapter
-        if (facilityName == "") {
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-            db.collection("facilities")
-//                    .whereEqualTo("EventID", eventID)
-                    .whereArrayContains("events", eventID)
-                    .get()
-
-                    .addOnCompleteListener(task -> {
-
-                        if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty()) {
-                            DocumentSnapshot document = task.getResult().getDocuments().get(0);
-
-                            String curr_facilityName = document.getString("name");
-                            String curr_facilityPictureURL = document.getString("pictureURL");
-
-                            Log.d("EventAdapter", "Found facility facilityName: " + curr_facilityName + " eventID: " + eventID);
-
-                            holder.facilityNameTextView.setText(curr_facilityName);
-
-                            if (curr_facilityPictureURL == null) {
-                                holder.facilityPicture.setImageDrawable(context.getResources().getDrawable(R.drawable.user));
-                                holder.facilityPicture.setImageTintList(context.getResources().getColorStateList(R.color.green));
-                            } else if (!curr_facilityPictureURL.isEmpty()) {
-                                holder.facilityPicture.setImageTintList(null);
-                                Glide.with(context).load(curr_facilityPictureURL).into(holder.facilityPicture);
-                            } //else {
-//                                holder.facilityPicture.setImageDrawable(context.getResources().getDrawable(R.drawable.user));
-//                                holder.facilityPicture.setImageTintList(context.getResources().getColorStateList(R.color.green));
-//                            }
-                        } else {
-                            Log.e("EventAdapter", "No facility found for EventID: " + eventID);
-                        }
-                    });
-        }
     }
-
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView facilityNameTextView;
         ImageView facilityPicture;
@@ -160,7 +121,6 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
         TextView eventStartTime;
         ImageView eventPicture;
         ImageView Delete;
-
         public MyViewHolder(View itemView){
             super(itemView);
             facilityNameTextView = itemView.findViewById(R.id.view_event_facility_name);
@@ -173,12 +133,10 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder
             Delete = itemView.findViewById(R.id.delete);
         }
     }
-
     @Override
     public int getItemCount() {
         return localEvents.size();
     }
-
     public interface OnEventClickListener {
         void onEventClick(String eventID);
     }
